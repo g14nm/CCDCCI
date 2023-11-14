55679d41baa03ccdc089a84f38ca7564b4c1e4e7
g14nm <gianmarcoprisa@hotmail.it>
2023-11-14 09:35:17 +0100

    modifiche classe Conto

--- src/it/betacom/model/Conto.java
+++ src/it/betacom/model/Conto.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,11 +16,12 @@ import it.betacom.util.FormattatoreDouble;
 public abstract class Conto {
 
 	private String titolare;
+	private LocalDate dataApertura;
 	private LocalDate dataChiusura;
 	private final double tassoInteresseGiornaliero;
 	private double interessiTotali;
 	private double saldo;
-	private List<Movimento> movimenti;
+	private Map<Integer, List<Movimento>> movimenti;
 	private Map<Integer, Double> interessiFineAnno;
 	
 	private static final int BONUS_APERTURA = 1000;
@@ -31,9 +31,10 @@ public abstract class Conto {
 	
 	public Conto(String titolare, LocalDate dataApertura, double tassoInteresseAnnuo) {
 		this.titolare = titolare;
+		this.dataApertura = dataApertura;
 		this.tassoInteresseGiornaliero = tassoInteresseAnnuo / 365;
 		this.interessiTotali = 0;
-		this.movimenti = new ArrayList<Movimento>();
+		this.movimenti = new HashMap<Integer, List<Movimento>>();
 		this.interessiFineAnno = new HashMap<Integer, Double>();
 		this.apri(dataApertura);
 	}
@@ -42,17 +43,18 @@ public abstract class Conto {
 		return titolare;
 	}
 	
-	public LocalDate getDataChiusura() {
-		return dataChiusura;
-	}
-	
 	public double getInteressiTotali() {
 		return this.interessiTotali;
 	}
 	
+	public LocalDate getDataChiusura() {
+		return this.dataChiusura;
+	}
+	
 	private void apri(LocalDate dataApertura) {
 		this.saldo = BONUS_APERTURA;
-		this.movimenti.add(new Movimento(TipoMovimento.APERTURA, dataApertura, BONUS_APERTURA, this.saldo));
+		this.movimenti.put(dataApertura.getYear(), new ArrayList<Movimento>());
+		this.movimenti.get(dataApertura.getYear()).add(new Movimento(TipoMovimento.APERTURA, dataApertura, BONUS_APERTURA, this.saldo));
 	}
 	
 	public void preleva(LocalDate data, double importo) {
@@ -72,33 +74,36 @@ public abstract class Conto {
 	}
 	
 	private void aggiungiMovimento(Movimento movimento) {
-		//controlla l'anno dell'ultimo movimento, se non è uguale al nuovo movimento
-		//viene aggiunto un movimento alla fine dell'anno precedente con il saldo corrente
-		//e poi aggiunto il nuovo movimento
-		int annoMovimentoPrecedente = this.movimenti.get(this.movimenti.size() - 1).getData().getYear();
-		if(!(annoMovimentoPrecedente == movimento.getData().getYear()))
-			this.movimenti.add(new Movimento(TipoMovimento.FINE_ANNO, LocalDate.of(annoMovimentoPrecedente, 12, 31), 0, this.saldo));
-		this.movimenti.add(movimento);
+		int annoMovimento = movimento.getData().getYear();
+		//se nella mappa dei movimenti non è presente la lista corrispondente all'anno del movimento
+		//viene aggiunto un movimento alla fine dell'anno precedente e viene istanziata la lista
+		if(movimenti.get(annoMovimento) == null) {
+			this.movimenti.get(annoMovimento - 1).add(new Movimento(TipoMovimento.FINE_ANNO, LocalDate.of(annoMovimento - 1, 12, 31), 0, this.saldo));
+			this.movimenti.put(annoMovimento, new ArrayList<Movimento>());
+		}
+		this.movimenti.get(annoMovimento).add(movimento);
 	}
 	
-	//per ogni movimento viene calcolato l'interesse generato nel periodo tra
-	//la data del movimento precedente e la data del movimento corrente e l'interesse
-	//viene aggiunto a interessiTotali
+	/** per ogni movimento viene calcolato l'interesse generato nel periodo tra
+	la data del movimento precedente e la data del movimento corrente e l'interesse
+	viene aggiunto al totale degli interessi **/
 	private void generaInteressi() {
-		this.movimenti.forEach(movimento -> {
-			if(!movimento.getTipoMovimento().equals(TipoMovimento.APERTURA)) {
-				Movimento movimentoPrecedente = movimenti.get(movimenti.indexOf(movimento) - 1);
-				double interesse = calcolaInteresseTraMovimenti(movimento, movimentoPrecedente);
+		int anno = this.dataApertura.getYear();
+		while(this.movimenti.keySet().contains(anno)) {
+			List<Movimento> movimentiAnno = this.movimenti.get(anno);
+			//se l'anno è quello dell'apertura del conto il ciclo parte dall'indice 1
+			for(int i = (anno == this.dataApertura.getYear()) ? 1 : 0; i < movimentiAnno.size(); i++) {
+				//se l'indice è 0 considera come movimento precedente l'ultimo dell'anno prima
+				Movimento movimentoPrecedente = (i == 0) 
+						? this.movimenti.get(anno - 1).get(this.movimenti.get(anno - 1).size() - 1)
+								: movimentiAnno.get(i - 1);
+				double interesse = calcolaInteresseTraMovimenti(movimentiAnno.get(i), movimentoPrecedente);
 				this.interessiTotali = FormattatoreDouble.formatta(this.interessiTotali + interesse);
-				this.interessiFineAnno.put(movimento.getData().getYear(), this.interessiTotali);
-				logger.debug("interesse tra " + movimentoPrecedente.getData().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) 
-						+ " e " + movimento.getData().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-						+ " : (" + movimentoPrecedente.getSaldoFinale()
-						+ " * " + (int)ChronoUnit.DAYS.between(movimentoPrecedente.getData(), movimento.getData())
-						+ " * " + tassoInteresseGiornaliero
-						+ ") / 100 = " + interesse);
+				this.logCalcoloInteresse(movimentoPrecedente.getData(), movimentiAnno.get(i).getData(), movimentoPrecedente.getSaldoFinale(), interesse);
 			}
-		});
+			this.interessiFineAnno.put(anno, this.interessiTotali);
+			anno++;
+		};
 	}
 	
 	private double calcolaInteresseTraMovimenti(Movimento corrente, Movimento precedente) {
@@ -110,17 +115,25 @@ public abstract class Conto {
 						) / 100);
 	}
 	
-	public List<Movimento> getMovimentiAnno(int anno){
-		return this.movimenti.stream().filter(movimento -> movimento.getData().getYear() == anno).collect(Collectors.toList());
+	private void logCalcoloInteresse(LocalDate inizioPeriodo, LocalDate finePeriodo, double saldoPrecedente, double interesse) {
+		logger.debug("interesse tra " + inizioPeriodo.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) 
+		+ " e " + finePeriodo.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+		+ " : (" + saldoPrecedente
+		+ " * " + (int)ChronoUnit.DAYS.between(inizioPeriodo, finePeriodo)
+		+ " * " + tassoInteresseGiornaliero
+		+ ") / 100 = " + interesse);
+	}
+	
+	public List<Movimento> getMovimentiAnno(int anno) {
+		return this.movimenti.get(anno);
 	}
 	
 	public double getSaldoAnno(int anno) {
 		if(anno == LocalDate.now().getYear()) return this.saldo;
-		return getMovimentiAnno(anno).get(getMovimentiAnno(anno).size() - 1).getSaldoFinale();
+		return this.movimenti.get(anno).get(this.movimenti.get(anno).size() - 1).getSaldoFinale();
 	}
 	
 	public double getInteressiLordiAnno(int anno) {
-		if(anno == LocalDate.now().getYear()) return this.interessiTotali;
 		return this.interessiFineAnno.get(anno);
 	}
 	
